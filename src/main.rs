5b08fde8aa6a64d390690f9f8d2f9b44d2f430cc
Artur Das <a.daschevici@gmail.com>
2025-07-03 15:46:37 +0200

    Add some basic endpoints for now with light work on top of the actix web hello world

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+
+async fn index() -> impl Responder {
+    format!("Hello, world!")
+}
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello, {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
