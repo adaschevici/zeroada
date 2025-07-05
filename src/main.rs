78ce7bb216bf999ca46e04c28118ea3fc6cdcff8
Artur Das <a.daschevici@gmail.com>
2025-07-06 00:23:26 +0200

    Add a wip endpoint for the healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn index() -> impl Responder {
     format!("Hello, world!")
@@ -8,11 +8,16 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello, {}!", name)
 }
 
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::NotImplemented().body("Health check not implemented")
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
+            .route("/health", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
