7011faf1fa07c4cb6604b27ba900754d0040e95b
Artur Das <a.daschevici@gmail.com>
2025-07-08 15:29:07 +0200

    Fix head requests being ignored although they should not be if you have a get handler defined

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
-async fn index() -> impl Responder {
-    format!("Hello, world!")
-}
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}!", name)
-}
-
 async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::NotImplemented().body("Health check not implemented")
+    HttpResponse::Ok().body("Ok")
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/health", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/health_check", web::head().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .bind("127.0.0.1:8091")?
     .run()
     .await
 }
